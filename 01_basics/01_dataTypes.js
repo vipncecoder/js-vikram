073252fce745899d68b417e0e0e624edaddef202
vipncecoder <153069596+vipncecoder@users.noreply.github.com>
2023-12-06 09:47:33 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //using this code will be treated as newer version of javaScript code
+// number 
+// BigInt 
+// String =>""
+// Boolean => true/false
+// null  => standalone value
+// undefined => 
+// Symbol => unique
+
+// Object
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //object
