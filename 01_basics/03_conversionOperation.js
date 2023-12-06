3dfb4c6ea1f06c30efdbee2265b2c9e911ea3eb4
vipncecoder <153069596+vipncecoder@users.noreply.github.com>
2023-12-06 10:03:42 +0000

    new datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+// let num=123;
+// console.log(num);
+// console.log(typeof(num));
+// let string= String(num);
+// console.log(typeof string);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0;
+
+// let isLoggedIn ="vikram"
+// let isLoggedIn ="1" // "0"  => true
+// let isLoggedIn =0 //=> false
+let isLoggedIn = 1; //=> true
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
