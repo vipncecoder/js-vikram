8454377c197faf0e8272c72f30eed88ed427e9c2
Vikram Kumar <153069596+vipncecoder@users.noreply.github.com>
2023-12-12 14:24:00 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// dataype should be same while comparing
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// '===' - strict check - means it will check for value and datatype both
+console.log("2" ===2);
