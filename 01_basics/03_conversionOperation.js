e72a3357476f60565fcced0329ee602b0523ab5a
Vikram Kumar <153069596+vipncecoder@users.noreply.github.com>
2023-12-12 14:13:19 +0000

    tricky conversion and operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,59 @@
-// let num=123;
-// console.log(num);
-// console.log(typeof(num));
-// let string= String(num);
-// console.log(typeof string);
+// // let num=123;
+// // console.log(num);
+// // console.log(typeof(num));
+// // let string= String(num);
+// // console.log(typeof string);
 
-//"33" => 33
-//"33abc" => NaN
-// true => 1; false => 0;
+// //"33" => 33
+// //"33abc" => NaN
+// // true => 1; false => 0;
 
-// let isLoggedIn ="vikram"
-// let isLoggedIn ="1" // "0"  => true
-// let isLoggedIn =0 //=> false
-let isLoggedIn = 1; //=> true
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// // let isLoggedIn ="vikram"
+// // let isLoggedIn ="1" // "0"  => true
+// // let isLoggedIn =0 //=> false
+// let isLoggedIn = 1; //=> true
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// **********************Operations***************************8
+//let value =3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 * 2);
+// console.log(2 - 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+
+//******Tricky conversion
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2"); // which is 1st will excute first in this case 1+2=3 then concatinate with "2" and will become 32
+console.log(1+"2"+2+"2"); //1222
+// https://tc39.es/ecma262/multipage/notational-conventions.html#sec-notational-conventions
+
+
+// increment decrement operator
+let x = 4;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:5, y:4"
+
+let a = 4;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:5, b:5"
+
+
+
+
+
+
+
