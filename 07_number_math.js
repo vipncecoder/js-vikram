ca3d877e97bb1dc00acbe8ec95744d4c9f8147e9
Vikram Kumar <153069596+vipncecoder@users.noreply.github.com>
2023-12-22 21:16:57 +0000

    maths

---
+++ 07_number_math.js
@@ -0,0 +1,31 @@
+const num1= 400;
+console.log(num1);
+const balance= new Number(100);
+console.log(balance);
+
+const num2=123.423;
+console.log(num2.toPrecision(3));
+
+const num3=10000000000;
+console.log(num3.toLocaleString(en-IN));
+
+
+// in console of browser we can also see what properties we have associated with strings, number etc
+/*
+function for number
+console.log(balance);
+VM694:1 Number {33}
+[[Prototype]]: Number
+constructor: ƒ Number()
+toExponential: ƒ toExponential()
+toFixed: ƒ toFixed()
+toLocaleString: ƒ toLocaleString()
+toPrecision: ƒ toPrecision()
+toString: ƒ toString()
+valueOf: ƒ valueOf()
+[[Prototype]]: Object
+[[PrimitiveValue]]: 0
+[[PrimitiveValue]]: 33
+
+*/
+
