a68f77b4bd1e4590da9937e76c9bfc000784c71a
Vikram Kumar <153069596+vipncecoder@users.noreply.github.com>
2024-03-06 07:48:26 +0000

    reduce,map and filter

--- 05_iterations/05_foreachloop.js
+++ 05_iterations/05_foreachloop.js
@@ -15,6 +15,75 @@ const coding=[ "js","cpp","py","ruby"];
 // }
 // coding.forEach(printMe);
 
-coding.forEach((item,index,arr)=>{
-    console.log(item,index,arr);
-})
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+// })
+
+// if we tag a var for foreach it will not store anything in that variable
+
+//   Filter
+
+const myNum=[1,2,3,4,5,6,7,8,9,10];
+// const newNums= myNum.filter((myNum)=>{   //filter takes call back function and can return values
+//     return myNum>4;     //here use of return is must 
+// })
+
+// console.log(newNums);
+
+// if we wanted to do the same we can use foreach loop as below
+
+// const newNums=[];
+// myNum.forEach((myNum)=>{
+//     if(myNum>4){
+//         newNums.push(myNum+2);
+//         // newNums.pop(myNum);
+//     }
+// })
+
+// console.log(newNums);
+
+// map
+
+// const number= myNum.map((num) => { return num+10})
+// console.log(number);
+
+// chaining of function
+
+const newNum= myNum.map((num)=> num*10).map((num)=> num+1).filter((num)=> num>50);
+// console.log(newNum);
+
+// reduce
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+// console.log(sumWithInitial);
+// Expected output: 10
+
+const shoppingCart= [
+    {
+        itemName : "js courese",
+        price :2999
+    },
+    {
+        itemName : "python courese",
+        price :3999
+    },
+    {
+        itemName : "java courese",
+        price :4999
+    },
+    {
+        itemName : "data science courese",
+        price :12999
+    }
+]
+
+const priceToPay= shoppingCart.reduce((acc, item)=> acc+item.price,0);
+console.log(priceToPay);
